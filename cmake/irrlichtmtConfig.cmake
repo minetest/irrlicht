d214e96bf802ea0ea88ec485e3c14f3363dd1c31
Josiah <josiah_vanderzee@mediacombb.net>
2021-03-27 12:14:04 -0500

    Proper config and version export.

--- cmake/irrlichtmtConfig.cmake
+++
@@ -1,8 +0,0 @@
-get_filename_component(IrrlichtMt_CMAKE_DIR "${CMAKE_CURRENT_LIST_DIR}" PATH)
-include(CMakeFindDependencyMacro)
-
-find_dependency(ZLIB REQUIRED)
-
-if(NOT TARGET irr::IrrlichtMt)
-    include("${IrrlichtMt_CMAKE_DIR}/irrlichtmt/irrlichtmtTargets.cmake")
-endif()
