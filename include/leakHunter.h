806665354dfc0796ef8835d6695a6c696315e4f0
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2021-08-25 17:23:16 +0000

    Fix include guard in leakHunter.h
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6238 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/leakHunter.h
+++ include/leakHunter.h
@@ -2,7 +2,8 @@
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in irrlicht.h
 
-#ifndef __LEAK_HUNTER_INCLUDEED__
+#ifndef __LEAK_HUNTER_INCLUDED__
+#define __LEAK_HUNTER_INCLUDED__
 
 #include "IrrCompileConfig.h"
 
