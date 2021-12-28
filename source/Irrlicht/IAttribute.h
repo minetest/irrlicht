8e0e7b5c43d70042cf33614b67f118b272caa320
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-12-28 20:11:04 +0100

    Trim down CAttributes and related files to only what is still used.

--- source/Irrlicht/IAttribute.h
+++ source/Irrlicht/IAttribute.h
@@ -48,61 +48,9 @@ public:
 
 	virtual s32 getInt() const				 { return 0; }
 	virtual f32 getFloat() const			 { return 0; }
-	virtual video::SColorf getColorf() const { return video::SColorf(1.0f,1.0f,1.0f,1.0f); }
-	virtual video::SColor getColor() const	 { return video::SColor(255,255,255,255); }
-	virtual core::stringc getString() const	 { return core::stringc(getStringW().c_str()); }
-	virtual core::stringw getStringW() const { return core::stringw(); }
-	virtual core::array<core::stringw> getArray() const { return core::array<core::stringw>(); };
-	virtual bool getBool() const			{ return false; }
-	virtual void getBinary(void* outdata, s32 maxLength) const {};
-	virtual core::vector3df getVector() const      { return core::vector3df(); }
-	virtual core::position2di getPosition()	const  { return core::position2di(); }
-	virtual core::rect<s32> getRect() const	       { return core::rect<s32>(); }
-	virtual core::quaternion getQuaternion() const { return core::quaternion(); }
-	virtual core::matrix4 getMatrix() const	      { return core::matrix4(); }
-	virtual core::triangle3df getTriangle() const { return core::triangle3df(); }
-	virtual core::vector2df getVector2d() const	  { return core::vector2df(); }
-	virtual core::vector2di getVector2di() const  { return core::vector2di(); }
-	virtual core::line2df getLine2d() const  { return core::line2df(); }
-	virtual core::line2di getLine2di() const { return core::line2di(); }
-	virtual core::line3df getLine3d() const  { return core::line3df(); }
-	virtual core::line3di getLine3di() const { return core::line3di(); }
-	virtual core::dimension2du getDimension2d() const { return core::dimension2du(); }
-	virtual core::aabbox3d<f32> getBBox() const	{ return core::aabbox3d<f32>(); }
-	virtual core::plane3df getPlane() const	    { return core::plane3df(); }
-
-	virtual video::ITexture* getTexture() const	{ return 0; }
-	virtual const char* getEnum() const	 { return 0; }
-	virtual void* getUserPointer() const { return 0; }
 
 	virtual void setInt(s32 intValue)		{};
 	virtual void setFloat(f32 floatValue)		{};
-	virtual void setString(const char* text)	{};
-	virtual void setString(const wchar_t* text){ setString(core::stringc(text).c_str()); };
-	virtual void setArray(const core::array<core::stringw>& arr )	{};
-	virtual void setColor(video::SColorf color)	{};
-	virtual void setColor(video::SColor color)	{};
-	virtual void setBool(bool boolValue)		{};
-	virtual void setBinary(void* data, s32 maxLength) {};
-	virtual void setVector(const core::vector3df& v)	{};
-	virtual void setPosition(const core::position2di& v)	{};
-	virtual void setRect(const core::rect<s32>& v)		{};
-	virtual void setQuaternion(const core::quaternion& v) {};
-	virtual void setMatrix(const core::matrix4& v) {};
-	virtual void setTriangle(const core::triangle3df& v) {};
-	virtual void setVector2d(const core::vector2df& v) {};
-	virtual void setVector2d(const core::vector2di& v) {};
-	virtual void setLine2d(const core::line2df& v) {};
-	virtual void setLine2d(const core::line2di& v) {};
-	virtual void setLine3d(const core::line3df& v) {};
-	virtual void setLine3d(const core::line3di& v) {};
-	virtual void setDimension2d(const core::dimension2du& v) {};
-	virtual void setBBox(const core::aabbox3d<f32>& v) {};
-	virtual void setPlane(const core::plane3df& v) {};
-	virtual void setUserPointer(void* v)	{};
-
-	virtual void setEnum(const char* enumValue, const char* const* enumerationLiterals) {};
-	virtual void setTexture(video::ITexture*, const path& filename)	{};
 
 	core::stringc Name;
 
