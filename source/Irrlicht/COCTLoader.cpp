9e27a6d4bfacd6b14d73abbfca455d81c881c999
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2022-01-08 17:30:06 +0000

    Avoid allocating more than 16k on stack in OCT loader. Also avoid potential heap overwrites in there. Sadly I have no examples for OCT files and it doesn't seem like a very common format as I couldn't even find any examples online. So just assuming my changes work.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6291 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COCTLoader.cpp
+++ source/Irrlicht/COCTLoader.cpp
@@ -102,18 +102,29 @@ IAnimatedMesh* COCTLoader::createMesh(io::IReadFile* file)
 
 	file->read(verts, sizeof(octVert) * header.numVerts);
 	file->read(faces, sizeof(octFace) * header.numFaces);
-	//TODO: Make sure id is in the legal range for Textures and Lightmaps
 
 	u32 i;
-	for (i = 0; i < header.numTextures; i++) {
-		octTexture t;
-		file->read(&t, sizeof(octTexture));
-		textures[t.id] = t;
+	for (i = 0; i < header.numTextures; i++) 
+	{
+		u32 id;
+		file->read(&id, sizeof(id));
+		if ( id >= header.numTextures )
+		{
+			os::Printer::log("COCTLoader: Invalid texture id", irr::ELL_WARNING);
+			id = i;
+		}
+		file->read(&textures[id], sizeof(octTexture));
 	}
-	for (i = 0; i < header.numLightmaps; i++) {
-		octLightmap t;
-		file->read(&t, sizeof(octLightmap));
-		lightmaps[t.id] = t;
+	for (i = 0; i < header.numLightmaps; i++) 
+	{
+		u32 id;
+		file->read(&id, sizeof(id));
+		if ( id >= header.numLightmaps )
+		{
+			os::Printer::log("COCTLoader: Invalid lightmap id", irr::ELL_WARNING);
+			id = i;
+		}
+		file->read(&lightmaps[id], sizeof(octLightmap));
 	}
 	file->read(lights, sizeof(octLight) * header.numLights);
 
