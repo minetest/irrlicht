01586f584aef460686c9ac7a59900ea90eb23fe8
sfan5 <sfan5@live.de>
2022-02-26 11:23:23 +0100

    Delete more old files

--- scripts/doc/irrlicht/maketutorial.bat
+++
@@ -1,4 +0,0 @@
-REM OUTDATED - don't use this anymore. For now only maketutorial.sh works, this one still has to be updated.
-REM type tut_head.html >%2
-REM ..\sed.exe -f maketut.sed %1 >>%2
-REM type tut_end.html >>%2
