64323abb38a00cf1aee9a43270d9ab70db6e5fa2
neverix <nev@neverix.io>
2022-12-08 23:14:44 +0300

    Update IrrCompileConfig.h

--- include/IrrCompileConfig.h
+++ include/IrrCompileConfig.h
@@ -45,7 +45,7 @@
 //! Example: NO_IRR_COMPILE_WITH_X11_ would disable X11
 
 //! Uncomment this line to compile with the SDL device
-//#define _IRR_COMPILE_WITH_SDL_DEVICE_
+#define _IRR_COMPILE_WITH_SDL_DEVICE_
 #ifdef NO_IRR_COMPILE_WITH_SDL_DEVICE_
 #undef _IRR_COMPILE_WITH_SDL_DEVICE_
 #endif
