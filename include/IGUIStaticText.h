c15e23aca558b005363e1aa43f661609f537be27
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2022-12-22 14:56:44 +0000

    Fix spelling of enums in header comments
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6445 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IGUIStaticText.h
+++ include/IGUIStaticText.h
@@ -88,9 +88,9 @@ namespace gui
 
 		//! Sets text justification mode
 		/** \param horizontal: EGUIA_UPPERLEFT for left justified (default),
-		EGUIA_LOWEERRIGHT for right justified, or EGUIA_CENTER for centered text.
+		EGUIA_LOWERRIGHT for right justified, or EGUIA_CENTER for centered text.
 		\param vertical: EGUIA_UPPERLEFT to align with top edge (default),
-		EGUIA_LOWEERRIGHT for bottom edge, or EGUIA_CENTER for centered text. */
+		EGUIA_LOWERRIGHT for bottom edge, or EGUIA_CENTER for centered text. */
 		virtual void setTextAlignment(EGUI_ALIGNMENT horizontal, EGUI_ALIGNMENT vertical) = 0;
 
 		//! Enables or disables word wrap for using the static text as multiline text control.
