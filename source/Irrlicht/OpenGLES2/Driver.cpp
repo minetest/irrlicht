03b13877272389b88fc7b67ebe69fcd059fc7aca
numzero <numzer0@yandex.ru>
2023-04-09 20:56:12 +0300

    OpenGL3: cleanup COpenGLES2Driver::readTexture

--- source/Irrlicht/OpenGLES2/Driver.cpp
+++ source/Irrlicht/OpenGLES2/Driver.cpp
@@ -21,66 +21,46 @@ namespace video {
 	}
 
 	bool COpenGLES2Driver::readTexture(IImage *into, COpenGL3Texture *texture) {
-		bool passed = true;
+		// GLES (even 3.2) can’t read textures to RAM, but it can read framebuffers to RAM. Use that.
 
 		auto Size = texture->getSize();
+		auto ColorFormat = texture->getColorFormat();
+		void (*Converter)(const void* sP, s32 sN, void* dP);
 
-		// TODO: on ES2 we can likely also work with glCopyTexImage2D instead of rendering which should be faster.
-		COpenGL3Texture* tmpTexture = new COpenGL3Texture("OGL_CORE_LOCK_TEXTURE", Size, ETT_2D, texture->getColorFormat(), this);
+		switch (ColorFormat)
+		{
+		case ECF_A1R5G5B5: Converter = CColorConverter::convert_A8R8G8B8toA1B5G5R5; break;
+		case ECF_R5G6B5:   Converter = CColorConverter::convert_A8R8G8B8toR5G6B5;   break;
+		case ECF_R8G8B8:   Converter = CColorConverter::convert_A8R8G8B8toB8G8R8;   break;
+		case ECF_A8R8G8B8: Converter = CColorConverter::convert_A8R8G8B8toA8B8G8R8; break;
+		default: return false;
+		}
 
 		GLuint tmpFBO = 0;
 		irrGlGenFramebuffers(1, &tmpFBO);
-
-		GLint prevViewportX = 0;
-		GLint prevViewportY = 0;
-		GLsizei prevViewportWidth = 0;
-		GLsizei prevViewportHeight = 0;
-		getCacheHandler()->getViewport(prevViewportX, prevViewportY, prevViewportWidth, prevViewportHeight);
-		getCacheHandler()->setViewport(0, 0, Size.Width, Size.Height);
-
 		GLuint prevFBO = 0;
-		getCacheHandler()->getFBO(prevFBO);
-		getCacheHandler()->setFBO(tmpFBO);
-
-		irrGlFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tmpTexture->getOpenGLTextureName(), 0);
-
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		draw2DImage(texture, 0, true);
+		CacheHandler->getFBO(prevFBO);
+		CacheHandler->setFBO(tmpFBO);
+		irrGlFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture->getOpenGLTextureName(), 0);
+		GLenum status = irrGlCheckFramebufferStatus(GL_FRAMEBUFFER);
+		if (status != GL_FRAMEBUFFER_COMPLETE) {
+			CacheHandler->setFBO(prevFBO);
+			irrGlDeleteFramebuffers(1, &tmpFBO);
+			os::Printer::log("readTexture: Texture format is not supported", ColorFormatNames[ColorFormat < ECF_UNKNOWN?ColorFormat:ECF_UNKNOWN], ELL_ERROR);
+			return false;
+		}
 
 		IImage* tmpImage = createImage(ECF_A8R8G8B8, Size);
 		glReadPixels(0, 0, Size.Width, Size.Height, GL_RGBA, GL_UNSIGNED_BYTE, tmpImage->getData());
 
-		getCacheHandler()->setFBO(prevFBO);
-		getCacheHandler()->setViewport(prevViewportX, prevViewportY, prevViewportWidth, prevViewportHeight);
-
+		CacheHandler->setFBO(prevFBO);
 		irrGlDeleteFramebuffers(1, &tmpFBO);
-		delete tmpTexture;
 
-		void* src = tmpImage->getData();
-		void* dest = into->getData();
+		Converter(tmpImage->getData(), tmpImage->getDimension().getArea(), into->getData());
 
-		switch (texture->getColorFormat())
-		{
-		case ECF_A1R5G5B5:
-			CColorConverter::convert_A8R8G8B8toA1B5G5R5(src, tmpImage->getDimension().getArea(), dest);
-			break;
-		case ECF_R5G6B5:
-			CColorConverter::convert_A8R8G8B8toR5G6B5(src, tmpImage->getDimension().getArea(), dest);
-			break;
-		case ECF_R8G8B8:
-			CColorConverter::convert_A8R8G8B8toB8G8R8(src, tmpImage->getDimension().getArea(), dest);
-			break;
-		case ECF_A8R8G8B8:
-			CColorConverter::convert_A8R8G8B8toA8B8G8R8(src, tmpImage->getDimension().getArea(), dest);
-			break;
-		default:
-			passed = false;
-			break;
-		}
 		tmpImage->drop();
 
-		return passed;
+		return true;
 	}
 }
 }
