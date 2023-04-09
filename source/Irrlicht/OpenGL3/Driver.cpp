56eeb79c529249ade367f543f5d1c875c3974f04
numzero <numzer0@yandex.ru>
2023-04-09 20:28:53 +0300

    OpenGL3: cleanup COpenGL3Driver::readTexture

--- source/Irrlicht/OpenGL3/Driver.cpp
+++ source/Irrlicht/OpenGL3/Driver.cpp
@@ -3,6 +3,8 @@
 // For conditions of distribution and use, see copyright notice in Irrlicht.h
 
 #include "Driver.h"
+#include <memory>
+#include <cstring>
 #include "OpenGL/Texture.h"
 #include "COpenGLCoreCacheHandler.h"
 
@@ -20,42 +22,37 @@ namespace video {
 		return driver;
 	}
 
-	bool COpenGL3Driver::readTexture(IImage *into, COpenGL3Texture *texture) {
-		IImage* tmpImage = into;	// not sure yet if the size required by glGetTexImage is always correct, if not we might have to allocate a different tmpImage and convert colors later on.
+	static void flipImageY(IImage *image) {
+		const s32 pitch = image->getPitch();
+		const u32 height = image->getDimension().Height;
 
-		getCacheHandler()->getTextureCache().set(0, texture);
+		u8* srcA = static_cast<u8*>(image->getData());
+		u8* srcB = srcA + (height - 1) * pitch;
+
+		std::unique_ptr<u8[]> tmpBuffer{new u8[pitch]};
+
+		for (u32 i = 0; i < height / 2; i++) {
+			memcpy(tmpBuffer.get(), srcA, pitch);
+			memcpy(srcA, srcB, pitch);
+			memcpy(srcB, tmpBuffer.get(), pitch);
+			srcA += pitch;
+			srcB -= pitch;
+		}
+	}
+
+	bool COpenGL3Driver::readTexture(IImage *into, COpenGL3Texture *texture) {
+		CacheHandler->getTextureCache().set(0, texture);
 		testGLError(__LINE__);
 
 		GLenum tmpTextureType = texture->getOpenGLTextureType();
-
 		if (tmpTextureType == GL_TEXTURE_CUBE_MAP)
-		{
 			tmpTextureType = GL_TEXTURE_CUBE_MAP_POSITIVE_X;
-		}
 
-		glGetTexImage(tmpTextureType, 0, texture->getPixelFormat(), texture->getPixelType(), tmpImage->getData());
+		glGetTexImage(tmpTextureType, 0, texture->getPixelFormat(), texture->getPixelType(), into->getData());
 		testGLError(__LINE__);
 
 		if (texture->isRenderTarget()) // FIXME this is due to ETLF_FLIP_Y_UP_RTT being forced on
-		{
-			const s32 pitch = tmpImage->getPitch();
-
-			u8* srcA = static_cast<u8*>(tmpImage->getData());
-			u8* srcB = srcA + (tmpImage->getDimension().Height - 1) * pitch;
-
-			u8* tmpBuffer = new u8[pitch];
-
-			for (u32 i = 0; i < tmpImage->getDimension().Height; i += 2)
-			{
-				memcpy(tmpBuffer, srcA, pitch);
-				memcpy(srcA, srcB, pitch);
-				memcpy(srcB, tmpBuffer, pitch);
-				srcA += pitch;
-				srcB -= pitch;
-			}
-
-			delete[] tmpBuffer;
-		}
+			flipImageY(into);
 
 		return true;
 	}
