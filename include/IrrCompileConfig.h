c311d01c073fa4655fa5ada8feb9d4346b64669d
sfan5 <sfan5@live.de>
2023-07-20 20:14:30 +0200

    Bump revision

--- include/IrrCompileConfig.h
+++ include/IrrCompileConfig.h
@@ -6,8 +6,8 @@
 #define __IRR_COMPILE_CONFIG_H_INCLUDED__
 
 //! Identifies the IrrlichtMt fork customized for the Minetest engine
-#define IRRLICHT_VERSION_MT_REVISION 11
-#define IRRLICHT_VERSION_MT "mt11"
+#define IRRLICHT_VERSION_MT_REVISION 12
+#define IRRLICHT_VERSION_MT "mt12"
 
 //! Irrlicht SDK Version
 #define IRRLICHT_VERSION_MAJOR 1
