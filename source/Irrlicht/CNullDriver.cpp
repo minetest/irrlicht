minetest/irrlicht

commit 7298b46504c109b13bab26c32d4b94f6985074d5

Use binary search in CNullDriver::removeTexture()

source/Irrlicht/CNullDriver.cpp
@@ -283,15 +283,13 @@ void CNullDriver::removeTexture(ITexture* texture)
 {
 	if (!texture)
 		return;
+	SSurface s;
+	s.Surface = texture;
 
-	for (u32 i=0; i<Textures.size(); ++i)
-	{
-		if (Textures[i].Surface == texture)
-		{
-			texture->drop();
-			Textures.erase(i);
-			return;
-		}
+	s32 index = Textures.binary_search(s);
+	if (index != -1) {
+		texture->drop();
+		Textures.erase(index);
 	}
 }
 

commit 679dfd33437ecb265cbe64d3663d9afe989e63d2

Fix CNullDriver::removeTexture() segfault
`Textures` is not an one-to-one mapping.
Minetest still crashes with this commit but that's because
it attempts to double-free a texture.
broken by 7298b46504c109b13bab26c32d4b94f6985074d5

source/Irrlicht/CNullDriver.cpp
@@ -286,10 +286,16 @@ void CNullDriver::removeTexture(ITexture* texture)
 	SSurface s;
 	s.Surface = texture;
 
-	s32 index = Textures.binary_search(s);
-	if (index != -1) {
-		texture->drop();
-		Textures.erase(index);
+	s32 last;
+	s32 first = Textures.binary_search_multi(s, last);
+	if (first == -1)
+		return;
+	for (u32 i = first; i <= (u32)last; i++) {
+		if (Textures[i].Surface == texture) {
+			texture->drop();
+			Textures.erase(i);
+			return;
+		}
 	}
 }
 