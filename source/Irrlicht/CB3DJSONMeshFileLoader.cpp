a3f07d10b738979279f18f641bd158fda753d8c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-26 02:53:55 -0500

    It works!

--- source/Irrlicht/CB3DJSONMeshFileLoader.cpp
+++ source/Irrlicht/CB3DJSONMeshFileLoader.cpp
@@ -1,5 +1,6 @@
+#include "CB3DJSONMeshFileLoader.h"
+#include "coreutil.h"
 #include "IAnimatedMesh.h"
-#include "IMeshLoader.h"
 #include "IReadFile.h"
 #include "path.h"
 #include "json/json.hpp"
@@ -10,20 +11,23 @@ namespace irr
 namespace scene
 {
 
-class CB3DJSONMeshFileLoader : public IMeshLoader
-{
-public:
-
+// Class methods.
 CB3DJSONMeshFileLoader::CB3DJSONMeshFileLoader()
 {
+
 }
 
 bool CB3DJSONMeshFileLoader::isALoadableFileExtension(
   const io::path& fileName) const
 {
+  //* we could do multiple accepted extensions, like b3dj jb3d, j3d, etc. Talk with core devs about this.
   return core::hasFileExtension(fileName, "b3djson");
 }
 
-};// class CB3DJSONMeshFileLoader
+IAnimatedMesh* CB3DJSONMeshFileLoader::createMesh(io::IReadFile* file) {
+  //! I'm sure this isn't a horrible idea!
+  return nullptr;
+}
+
 } // namespace scene
 } // namespace irr
