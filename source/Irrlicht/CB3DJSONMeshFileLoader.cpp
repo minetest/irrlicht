6406dbb9df0c2ce467e23b5dfffa1abc40841c78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-26 01:48:13 -0500

    Wow, there's still nothing, incredible

--- source/Irrlicht/CB3DJSONMeshFileLoader.cpp
+++ source/Irrlicht/CB3DJSONMeshFileLoader.cpp
@@ -3,3 +3,24 @@
 #include "IReadFile.h"
 #include "path.h"
 #include "json.hpp"
+
+namespace irr
+{
+
+namespace scene
+{
+
+class CB3DJSONMeshFileLoader : public IMeshLoader
+{
+public:
+
+  CB3DJSONMeshFileLoader();
+
+  bool isALoadableFileExtension(const io::path& fileName) const override;
+
+  IAnimatedMesh* createMesh(io::IReadFile* file) override;
+
+
+};// class CB3DJSONMeshFileLoader
+} // namespace scene
+} // namespace irr
