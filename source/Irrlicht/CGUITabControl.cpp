952f11f806eb1bab853a60de3cd5b07fdb08b645
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2023-12-05 13:40:33 +0000

    Fix: CGUITabControl now catching EMIE_LMOUSE_PRESSED_DOWN Same as most elements. Other behaviour a bit unexpected and unlikely to be useful.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6576 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CGUITabControl.cpp
+++ source/Irrlicht/CGUITabControl.cpp
@@ -448,9 +448,9 @@ bool CGUITabControl::OnEvent(const SEvent& event)
 		case EET_MOUSE_INPUT_EVENT:
 			switch(event.MouseInput.Event)
 			{
-			//case EMIE_LMOUSE_PRESSED_DOWN:
+			case EMIE_LMOUSE_PRESSED_DOWN:
 			//	// todo: dragging tabs around
-			//	return true;
+				return true;
 			case EMIE_LMOUSE_LEFT_UP:
 			{
 				s32 idx = getTabAt(event.MouseInput.X, event.MouseInput.Y);
