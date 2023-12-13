5a37ef928e066d5a64cfaf0aa24ed3228f8c946f
sfan5 <sfan5@live.de>
2023-12-13 12:37:40 +0100

    MinGW toolchain refresh

--- scripts/ci-get-mingw.sh
+++ scripts/ci-get-mingw.sh
@@ -3,8 +3,8 @@ set -e
 topdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -z "$1" ] && exit 255
 
-ver=11.2.0
-os=ubuntu20.04
+ver=13.2.0
+os=ubuntu22.04
 name="mingw-w64-${1}_${ver}_${os}.tar.xz"
 wget "http://minetest.kitsunemimi.pw/$name" -O "$name"
 sha256sum -w -c <(grep -F "$name" "$topdir/sha256sums.txt")
