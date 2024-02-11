330150854b67013073c5c1dd735cf1963592ed81
Fatih Uzunoglu <fuzun54@outlook.com>
2024-02-11 19:21:06 +0200

    SDL: set default for ActiveIcon
    getActiveIcon() may be called before ActiveIcon
    is initialized, leading to undefined behavior.

--- source/Irrlicht/CIrrDeviceSDL.h
+++ source/Irrlicht/CIrrDeviceSDL.h
@@ -260,7 +260,7 @@ namespace irr
 				}
 			};
 			std::vector<std::unique_ptr<SDL_Cursor, CursorDeleter>> Cursors;
-			gui::ECURSOR_ICON ActiveIcon;
+			gui::ECURSOR_ICON ActiveIcon = gui::ECURSOR_ICON::ECI_NORMAL;
 		};
 
 	private:
